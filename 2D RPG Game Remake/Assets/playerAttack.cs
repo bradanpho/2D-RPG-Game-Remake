77b8b4767873693926fcf74e5e91212c48ae997f
bradan <bradanpho@gmail.com>
2025-05-19 16:41:38 -0500

    punches
    a lil buggy

--- 2D RPG Game Remake/Assets/playerAttack.cs
+++ 2D RPG Game Remake/Assets/playerAttack.cs
@@ -6,16 +6,20 @@ public class playerAttack : MonoBehaviour
 {
     private Animator animator;
     private bool isAttacking = false;
+    private playerMovement playerMovement; // Reference to movement script
+
     [SerializeField] private float attackCooldown = 0.4f;
 
     void Start()
     {
         animator = GetComponent<Animator>();
+        playerMovement = GetComponent<playerMovement>(); // Get movement component
     }
 
-    public void OnFire(InputAction.CallbackContext context)
+    void Update()
     {
-        if (context.performed && !isAttacking)
+        // Check if left mouse button is pressed this frame
+        if (Mouse.current.leftButton.wasPressedThisFrame && !isAttacking)
         {
             StartCoroutine(PerformAttack());
         }
@@ -24,23 +28,29 @@ public class playerAttack : MonoBehaviour
     private IEnumerator PerformAttack()
     {
         isAttacking = true;
+
+        // Get last movement direction for attack
+        Vector2 attackDirection = playerMovement.LastMoveDir;
+
+        // Assign values to Blend Tree parameters
+        animator.SetFloat("attackX", attackDirection.x);
+        animator.SetFloat("attackY", attackDirection.y);
+
+        // Trigger attack animation
         animator.SetTrigger("isAttacking");
 
         // Optional: disable movement during attack
         GetComponent<playerMovement>().enabled = false;
 
-        // Wait for the animation to finish or cooldown
         yield return new WaitForSeconds(attackCooldown);
 
         // Re-enable movement
         GetComponent<playerMovement>().enabled = true;
-
         isAttacking = false;
     }
     public void attackEnd()
     {
-        // Called by animation event at the end of attack animation
-        GetComponent<playerMovement>().enabled = true;  // Re-enable movement if you disabled it
+        GetComponent<playerMovement>().enabled = true;
         isAttacking = false;
     }
 
